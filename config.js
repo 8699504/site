b41b51c3b2c7793256529888cfbcf6bf57358af2
8699504 <101637860+8699504@users.noreply.github.com>
2022-03-17 14:17:33 +0300

    a

---
+++ config.js
@@ -0,0 +1,81 @@
+const config = {
+  gatsby: {
+    pathPrefix: '/',
+    siteUrl: 'https://nevsehir.info',
+    gaTrackingId: null,
+    trailingSlash: false,
+  },
+  header: {
+    logo: 'https://arizakod.com/wp-content/uploads/2022/03/logo.png',
+    logoLink: 'https://coinhane.net/',
+    title:
+      "<a href='https://coinhane.net/'><img class='img-responsive' src='https://arizakod.com/wp-content/uploads/2022/03/logo.png' alt='Coinhane' /></a>",
+    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
+    helpUrl: '',
+    tweetText: '',
+    social: `<li>
+		    <a href="#" target="_blank" rel="noopener">
+		      <div class="twitterBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
+		      </div>
+		    </a>
+		  </li>
+			<li>
+		    <a href="#" target="_blank" rel="noopener">
+		      <div class="discordBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
+		      </div>
+		    </a>
+		  </li>`,
+    links: [{ text: '', link: '' }],
+    search: {
+      enabled: false,
+      indexName: '',
+      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+    },
+  },
+  sidebar: {
+    forcedNavOrder: [
+      '/introduction', // add trailing slash if enabled above
+      '/codeblock',
+    ],
+    collapsedNav: [
+      '/codeblock', // add trailing slash if enabled above
+    ],
+    links: [{ text: 'Coinhane', link: 'https://coinhane.net' }],
+    frontline: false,
+    ignoreIndex: true,
+    title:
+      "<a href='https://nevsehir.info/'>nevsehir </a><div class='greenCircle'></div><a href='https://arizakod.com'>arıza kodu</a>",
+  },
+  siteMetadata: {
+    title: 'Gatsby Gitbook Boilerplate | Hasura',
+    description: 'Documentation built with mdx. Powering nevsehir.info ',
+    ogImage: null,
+    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
+    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+  },
+  pwa: {
+    enabled: false, // disabling this will also remove the existing service worker.
+    manifest: {
+      name: 'Gatsby Gitbook Starter',
+      short_name: 'GitbookStarter',
+      start_url: '/',
+      background_color: '#6b37bf',
+      theme_color: '#6b37bf',
+      display: 'standalone',
+      crossOrigin: 'use-credentials',
+      icons: [
+        {
+          src: 'src/pwa-512.png',
+          sizes: `512x512`,
+          type: `image/png`,
+        },
+      ],
+    },
+  },
+};
+
+module.exports = config;
